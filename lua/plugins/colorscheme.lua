66e1e75d9febb69840eb8d8b2bf02eb4efdc78f0
Dat Do Duc <doducdat2904@gmail.com>
2024-12-27 23:04:19 +0700

    disable noice plugin + added some keybind

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,13 @@
 return {
   -- { "rose-pine/nvim", name = "rose-pine" },
   -- { "ellisonleao/gruvbox.nvim" },
-  { "craftzdog/solarized-osaka.nvim" },
+  -- { "craftzdog/solarized-osaka.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      -- colorscheme = "solarized-osaka",
+      -- colorscheme = "habamax",
+      colorscheme = "default",
     },
   },
 }
